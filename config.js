6576bd86e70173207d593724fae5c8d7ead747b5
Roland Vachter <roland.vachter@iquestgroup.com>
2016-05-05 10:04:58 +0300

    Default config transformed into js file.

---
+++ config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    suds: {
+        baseUrl: "//session-user-data.webservices.ft.com",
+        endpoints: {
+            livefyre: {
+                init: "/v1/livefyre/init",
+                commentCount: "/v1/livefyre/commentcount"
+            },
+            user: {
+                updateUser: "/v1/user/updateuser",
+                getAuth: "/v1/user/getauth"
+            }
+        }
+    },
+    ccs: {
+        baseUrl: "//comment-creation.webservices.ft.com",
+        endpoints: {
+            getComments: "/v1/getComments",
+            postComment: "/v1/postComment",
+            deleteComment: "/v1/deleteComment"
+        }
+    },
+    cacheConfig: {
+        authBaseName: "comments-prod-auth-",
+        initBaseName: "comments-prod-init-"
+    },
+    livefyre: {
+        networkName: "ft"
+    }
+};
